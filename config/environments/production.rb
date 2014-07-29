19a70b4354f10936340d8239039731397d3f0e84
RiseShia <rise.shia@gmail.com>
2014-07-29 22:34:06 +0900

    add config.eager_load option to environment setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ Bonghwa::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+  # eager load code on boot.
   config.eager_load = true
   
   #log file changes day by day

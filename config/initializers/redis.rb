b8cd4031a97ac9c3f1c9d4931923411b658bea7f
Re.Shia <rise.shia@gmail.com>
2014-08-18 09:02:41 +0900

    Delete redis.rb
    This is local setting

--- config/initializers/redis.rb
+++
@@ -1,8 +0,0 @@
-$redis = Redis.new(:host => 'localhost', :port => 6379)
-$servername = "msm"
-$redis.del("#{$servername}:fws")
-$redis_cache_size = 500
-#Preload Firewoods
-Firewood.all.order("id DESC").limit($redis_cache_size).each do |fw|
-  $redis.zadd("#{$servername}:fws",fw.id,Marshal.dump(fw))
-end

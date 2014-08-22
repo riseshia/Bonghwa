f3bb22e12b208952f9c5cee79bdc7dbd612f4d71
RiseShia <rise.shia@gmail.com>
2014-08-23 01:50:21 +0900

    #10 is cleared. now it will be focused to ID field after loading page

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,17 @@
 FactoryGirl.define do
-	factory :user do
-		login_id	"user_id"
-		name 			"user_name"
-		password	"userpwd"
-		password_confirmation "userpwd"
+  factory :user do
+    login_id  "user_id"
+    name      "user_name"
+    password  "userpwd"
+    password_confirmation "userpwd"
     level     "1"
-	end
+  end
+
+  factory :admin, class: User do
+    login_id  "admin"
+    name      "administrator"
+    password  "userpwd"
+    password_confirmation "userpwd"
+    level     "999"  
+  end
 end

9510d56b31cc199696898ce4a87fc5ebfb4ea484
Re.Shia <rise.shia@gmail.com>
2014-12-13 13:56:57 +0900

    ajax delete fw enabled

--- app/assets/javascripts/app/views/firewood-view.js
+++ app/assets/javascripts/app/views/firewood-view.js
@@ -8,11 +8,32 @@ var app = app || {};
 
     template: _.template($('#fw-template').html()),
 
+    events: {
+      'click .delete': 'delete'
+    },
+
     render: function () {
       this.$el.html(this.template(this.model.toJSON()));
-      this.$el.addClass("list-group-item div-firewood");
+      this.$el.addClass('list-group-item div-firewood');
 
       return this;
+    },
+
+    delete: function () {
+      var self = this;
+      var dataId = this.model.get('id');
+      var really = confirm('정말 삭제하시겠어요?');
+
+      if ( !really )
+        return false;
+
+      $.ajax({
+        url: '/api/destroy?id=' + dataId,
+        type: 'delete',
+        dataType: 'json'
+      }).then(function () {
+        self.remove();
+      });
     }
   });
 })(jQuery);

4df96f76bc5c6fbbf3d780015e9310f521cdcef7
Re.Shia <rise.shia@gmail.com>
2014-12-15 16:02:00 +0900

    mt expend enabled

--- app/assets/javascripts/app/views/firewood-view.js
+++ app/assets/javascripts/app/views/firewood-view.js
@@ -7,10 +7,12 @@ var app = app || {};
     tagName: 'li',
 
     template: _.template($('#fw-template').html()),
+    mtTemplate: _.template($('#mt-template').html()),
 
     events: {
       'click .delete': 'delete',
-      'click .mt-clk': 'clkUsername'
+      'click .mt-clk': 'clkUsername',
+      'click .mt-to': 'unFold'
     },
 
     render: function () {
@@ -42,6 +44,48 @@ var app = app || {};
       
       var arr = [this.model.get('name')];
       app.BWClient.trigger('form:appendMt', arr);
+    },
+
+    unFold: function (e) {
+      e.preventDefault();
+
+      var prev_id = this.model.get('prev_mt');
+      if ( prev_id == 0 ) {
+        return this;
+      }
+
+      // find previous mt at most 5
+      var fws = [];
+      var tmp = this;
+      while ( true ) {
+        tmp = app.firewoods.findWhere({id: prev_id});
+        fws.push(tmp);
+        prev_id = tmp.get('prev_mt');
+
+        if (prev_id == 0 && list < 5) {
+          break;
+        }
+      }
+
+      $self = this.$el;
+      $self.removeClass('mt-to')
+           .addClass('mt-open')
+           .find('.fw-main')
+           .after('<div class="loading" style="display:none;">로딩중입니다.</div>');
+      $self.find('.loading')
+           .slideDown(200);
+
+      var $body = $('<li class="list-group-item div-mention">');
+      _.each(fws, function (fw) {
+        $body.append(this.mtTemplate(fw));
+      }, this);
+
+      $self.find('.loading').remove();
+      $self.find('.fw-sub')
+           .html($body.html())
+           .slideDown(200);
+
+      return this;
     }
   });
 })(jQuery);

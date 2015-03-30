9378eeee41e1766cd5c081e89f1649a969fe1538
Shia <rise.shia@gmail.com>
2015-03-30 20:26:53 +0900

    #33 탭 변경하면 Navbar, Navmenu 모두 active가 변경되도록 수정

--- app/assets/javascripts/app/routers/router.js
+++ app/assets/javascripts/app/routers/router.js
@@ -12,16 +12,22 @@ var app = app || {};
 
     typeNow: function () {
       window.PAGE_TYPE = 1;
+      $('.all_nav').parent().removeClass('active')
+      $('.now_nav').parent().addClass('active');
       app.channel.load();
     },
 
     typeMt: function () {
       window.PAGE_TYPE = 2;
+      $('.all_nav').parent().removeClass('active')
+      $('.mt_nav').parent().addClass('active');
       app.channel.load();
     },
 
     typeMe: function () {
       window.PAGE_TYPE = 3;
+      $('.all_nav').parent().removeClass('active')
+      $('.me_nav').parent().addClass('active');
       app.channel.load();
     }
   });

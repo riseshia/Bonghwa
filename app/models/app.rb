5f0e1235f3b4ba783fe71aa244c1aac4b6ed6fd3
Shia <rise.shia@gmail.com>
2016-01-17 15:43:17 +0900

    Add App spec. - Active Record Validation

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,4 +1,6 @@
 # App
 class App < ActiveRecord::Base
   attr_accessible :app_name, :home_link, :home_name, :show_widget, :use_script, :widget_link
+
+  validates :app_name, presence: true
 end
---
+++ spec/models/app_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe App, type: :model do
+  describe 'Active Record Validations' do
+    it { should validate_presence_of(:app_name) }
+  end
+end

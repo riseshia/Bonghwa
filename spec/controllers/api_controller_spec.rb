8c6cb2032c0d377e39360cc93477d4a55fc0eb57
Shia <rise.shia@gmail.com>
2016-01-20 22:06:17 +0900

    add clean redis after controller test example to remove side effect among tests

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -7,17 +7,32 @@ RSpec.describe ApiController, type: :controller do
 
   context 'Not Logined' do
     it 'has a 302 status code' do
-      post :new, { firewood: attributes_for(:normal_message) }
+      post :new, firewood: attributes_for(:normal_message)
       expect(response.status).to eq(302)
     end
   end
 
   context 'Logined with normal User' do
+    before(:example) do
+      sign_in :user
+    end
+
+    after(:example) do
+      $redis.zremrangebyrank("#{$servername}:fws", 0, 0)
+    end
+
     describe 'POST #new' do
       it 'has a 200 status code' do
-        sign_in :user
-        post :new, { firewood: attributes_for(:normal_message) }
+        post :new, firewood: attributes_for(:normal_message)
         expect(response.status).to eq(200)
+      end
+
+      it 'create new firewood' do
+        expect do
+          post :new, firewood: attributes_for(:normal_message)
+        end.to change(Firewood, :count).by(1)
+
+        expect($redis.zrevrange("#{$servername}:fws", 0, 500).size).to eq(1)
       end
     end
   end

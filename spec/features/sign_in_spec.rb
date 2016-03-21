f86658af146378dc847c7f525ff03fc4e518fe65
Shia <rise.shia@gmail.com>
2016-03-22 06:34:28 +0900

    #30 add sign up feature test

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -3,10 +3,35 @@ require "rails_helper"
 
 RSpec.describe "SignIn", type: :feature do
   context "Not Logined" do
-    it 'has redirect to session#new' do
+    before(:each) do
       create(:app)
+      create(:user)
+    end
+
+    it "has redirect to session#new" do
       visit "/"
       expect(page).to have_content "Please sign in"
+    end
+
+    it "has fail to sign in" do
+      visit "/"
+      within("#login_form") do
+        fill_in "login_id", with: "NotExist"
+        fill_in "password", with: "NotExist"
+      end
+      click_button "Sign in"
+      expect(page).to have_content "아이디/비밀번호가 올바르지 않습니다."
+      expect(page).to have_content "Please sign in"
+    end
+
+    it "has success to sign in" do
+      visit "/"
+      within("#login_form") do
+        fill_in "login_id", with: "user_id"
+        fill_in "password", with: "userpwd"
+      end
+      click_button "Sign in"
+      expect(page).to have_css("div#firewoods")
     end
   end
 end

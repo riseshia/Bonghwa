4724c82ca16fc38e0e234d3b74f0bcebb8938f03
Shia <rise.shia@gmail.com>
2016-05-11 21:07:14 +0900

    remove unused index

--- db/migrate/20160510113016_add_devise_to_users.rb
+++ db/migrate/20160510113016_add_devise_to_users.rb
@@ -36,7 +36,7 @@ class AddDeviseToUsers < ActiveRecord::Migration
     end
 
     # add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end

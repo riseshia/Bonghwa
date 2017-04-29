9927cf322ea8ec30afba5732408200dc5017e005
Shia <rise.shia@gmail.com>
2017-04-29 12:07:11 +0900

    Remove keymap.js & implement simple one

--- frontend/src/Keymap.js
+++ frontend/src/Keymap.js
@@ -1,17 +1,44 @@
-import hotkeys from "hotkeys-js"
 import Actions from "./Actions"
+
+const keymaps = {
+  2: 50,
+  3: 51,
+  4: 52
+}
+
+const addEvent = (obj, event, method) => {
+  if (obj.addEventListener) {
+    obj.addEventListener(event, method, false)
+  } else if (obj.attachEvent) {
+    obj.attachEvent(`on${event}`, () => { method(window.event) })
+  }
+}
+
+const triggers = []
+const addTrigger = (keyCode, callback) => {
+  triggers.push({ key: keyCode, func: callback })
+}
 
 export default {
   register() {
-    hotkeys("2", () => {
+    addEvent(document, "keyup", (event) => {
+      const pressedKey = event.keyCode || event.which || event.charCode
+
+      triggers
+        .filter(({ key }) => (key === pressedKey))
+        .forEach(({ func }) => {
+          func(event)
+        })
+    })
+
+    addTrigger(keymaps["2"], () => {
       Actions.toggleGlobalOption("isImageAutoOpen")
     })
-    hotkeys("3", () => {
+    addTrigger(keymaps["3"], () => {
       Actions.toggleGlobalOption("isLiveStreaming")
     })
-    hotkeys("4", () => {
+    addTrigger(keymaps["4"], () => {
       Actions.focusForm()
-      return false
     })
   }
 }

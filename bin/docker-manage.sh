25d82b7bd10b758d733f5ad11c83872c5cf369f2
Shia <rise.shia@gmail.com>
2017-06-02 22:34:09 +0900

    Use rails binary directly on docker (#501)

--- bin/docker-manage.sh
+++ bin/docker-manage.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker exec -it $(docker ps -aqf "name=bonghwa_backend") bin/rails "$@"
+docker exec -it $(docker ps -aqf "name=bonghwa_backend") rails "$@"
--- bin/docker-startup.sh
+++ bin/docker-startup.sh
@@ -2,4 +2,4 @@
 
 rm -f tmp/pids/server.pid
 bundle install -j3 --quiet --path vendor/bundle
-bin/rails server -p 8000 -b 0.0.0.0
+rails server -p 8000 -b 0.0.0.0
